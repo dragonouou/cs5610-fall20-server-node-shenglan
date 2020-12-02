743a0a7e3ac077545f8b202062f7694234327b47
dragonouou <shenglanouou@gmail.com>
2020-12-01 23:44:28 -0800

    add port

--- server.js
+++ server.js
@@ -15,4 +15,14 @@ app.use(function (req,res,next) {
 
 require("./controllers/question.controller.server")(app)
 require("./controllers/quizzes.controller.server")(app)
-app.listen(3000)
+// app.listen(3000)
+
+
+// const express = require('express');
+// const path = require('path');
+// // const app = express();
+// app.use(express.static(__dirname + '/dist/cs5610-fall20-server-node-shenglan'));
+// app.get('/*', function(req,res) {
+//     res.sendFile(path.join(__dirname+
+//         '/dist/cs5610-fall20-client-angular-shenglan/index.html'));});
+app.listen(process.env.PORT || 3000);
